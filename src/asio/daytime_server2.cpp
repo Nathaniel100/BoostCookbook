5227f8fa747360f4c9481de1122cec7b2613f5bb
吴凡 <wufan@wufandeMacBook-Pro-2.local>
2017-03-16 18:16:43 +0800

    修复shared_from_this提示问题

--- src/asio/daytime_server2.cpp
+++ src/asio/daytime_server2.cpp
@@ -8,6 +8,7 @@
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
 #include <iostream>
 
 using boost::asio::ip::tcp;
